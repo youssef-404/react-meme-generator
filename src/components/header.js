f3a292c67eee247208ae4e956174757a786b5fa2
youssef-404 <eyoussef081@gmail.com>
2023-01-15 19:07:42 +0100

    header and form

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+
+
+export default function Header(){
+    return(
+        <header>
+            <img src={require("../images/troll-face.png")}/>
+            <h1>Meme Generator</h1>
+            <p>React Course - Project 3</p>
+        </header>
+    )
+}
---
+++ src/components/meme.js
@@ -0,0 +1,12 @@
+
+export default function Meme(){
+    return(
+        <form>
+            <div className="inputs"> 
+                <input placeholder="Top text"/>
+                <input placeholder="Bottom text"/>
+            </div>
+            <button>Get a new meme image  🖼</button>
+        </form>
+    )
+}
