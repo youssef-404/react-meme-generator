1505515984670e0648e75a2250fcad741875049a
youssef-404 <eyoussef081@gmail.com>
2023-01-25 19:00:33 +0100

    final version

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,22 +1,4 @@
-import memesData from "../memesData"
 import React from "react"
-
- /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
-     * 
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-     * 
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
-     * 
-     * Lastly, update the `getMemeImage` function and the markup 
-     * to reflect our newly reformed state object and array in the
-     * correct way.
-     */
     
 
 export default function Meme(){
@@ -27,28 +9,59 @@ export default function Meme(){
         randomImage:'https://i.imgflip.com/1ur9b0.jpg'
     })
 
-    const [allMemeImage,setAllMemeImage] = React.useState(memesData)
+    const [allMemes,setAllMemes] = React.useState([])
 
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
+    
     function getMemeImage() {
-        const memesArray = allMemeImage.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length) 
+        const randomNumber = Math.floor(Math.random() * allMemes.length) 
         setMeme((prevMeme)=>({
             ...prevMeme,
-            randomImage:memesArray[randomNumber].url
+            randomImage:allMemes[randomNumber].url
         })
         )
+    }
+
+    function handleChange(event){
+        const {name,value} = event.target
+        setMeme((prevData)=>({
+            ...prevData,
+            [name]:value
+        }))
     }
     return(
         <main>
 
-        <div className="form">
-            <div className="inputs"> 
-                <input placeholder="Top text"/>
-                <input placeholder="Bottom text"/>
+            <div className="form">
+                <div className="inputs"> 
+                    <input 
+                        type="text"
+                        placeholder="Top text"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                    <input 
+                        placeholder="Bottom text"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button onClick={getMemeImage}>Get a new meme image  🖼</button>
             </div>
-            <button onClick={getMemeImage}>Get a new meme image  🖼</button>
-        </div>
-            <img src={meme.randomImage}/>
+            <div className="memeContainer">
+                <img className="memeContainer--img" src={meme.randomImage} alt="meme"/>
+                <h2 className="punchline punchline--top">{meme.topText}</h2>
+                <h2 className="punchline punchline--bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
